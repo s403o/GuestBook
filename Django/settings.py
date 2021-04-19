e9e8108734cf167e45842dd35b992f7d23de5039
s403o <eslam.adel.me@gmail.com>
2021-04-19 02:22:16 +0200

    commit!

--- Django/settings.py
+++ Django/settings.py
@@ -75,13 +75,12 @@ WSGI_APPLICATION = 'Django.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
+import dj_database_url 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-    import dj_database_url 
     prod_db  =  dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(prod_db)
 }
