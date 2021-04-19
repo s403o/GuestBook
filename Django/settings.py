06c67d408e6bd8378706f0907aa1552ace817bec
s403o <eslam.adel.me@gmail.com>
2021-04-19 02:24:36 +0200

    commit!

--- Django/settings.py
+++ Django/settings.py
@@ -74,18 +74,17 @@ WSGI_APPLICATION = 'Django.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-import dj_database_url 
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-    prod_db  =  dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update(prod_db)
 }
 
-
+import dj_database_url
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
